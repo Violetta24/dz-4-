d9b0455427216c8124c6af40a5c1b8f826550b97
Viola <asdfg2009-29-69@mail.ru>
2022-08-18 19:31:27 +0300

    pflfxf 2

---
+++ LZ1/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 21
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+Console.Clear();
+
+Console.WriteLine("ВВедите точки координат");
+Console.Write("Ax:  ");
+int ax = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Ay:  ");
+int ay = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Az:  ");
+int az = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Bx:  ");
+int bx = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("By:  ");
+int by = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Bz:  ");
+int bz = Convert.ToInt32(Console.ReadLine());
+
+double pif = Math.Sqrt(Math.Pow((ax - bx), 2) + Math.Pow((ay - by), 2) + Math.Pow((az - bz), 2));  //Sqrt - корень  Pow - квадрат
+
+Console.WriteLine(Math.Round(pif, 2)); //на сколько отступать
+
+
+
---
+++ LZ3/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.Clear();
+
+Console.WriteLine("Введите число N");
+Console.Write("N:  ");
+
+int num = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+
+while (i <= num)
+{
+
+    Console.WriteLine($"  |{i,4}  |   {(Math.Pow((i), 3)),4} |");      //запутался в скобках (Math.Pow((i), 3)) равно как (i * i * i)
+
+    i++;
+}
+
+
